468d11ae40c7549e38e9122736c8962b928f337d
Chin <chinzhiweiblank@gmail.com>
2020-03-21 18:35:14 +0800

    Add backend, remove redundant flutter app

--- google_maps_in_flutter/lib/main.dart
+++ google_maps_in_flutter/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(1.3521, 103.8198);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
